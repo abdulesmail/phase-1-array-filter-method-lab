50bd13a88f4ae2eff0511d94e4fad740a239f38a
abdulesmail <abdul.esmail@student.moringaschool.com>
2023-10-12 22:51:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  const lowercaseName = name.toLowerCase();
+  const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === lowercaseName);
+  return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, query) {
+  const matchingDrivers = drivers.filter(driver => driver.startsWith(query));
+  return matchingDrivers;
+}
+
+function matchName(drivers, name) {
+  const matchingDrivers = drivers.filter(driver => driver.name === name);
+  return matchingDrivers;
+}
